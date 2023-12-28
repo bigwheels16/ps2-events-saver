759bf5122ecef6ec9c512a0291c2c9f50f05adf7
Tyrence <email1@jkbff.com>
2023-12-28 01:25:50 -0700

    use real metric now

--- src/main.py
+++ src/main.py
@@ -7,44 +7,7 @@ import websocket
 import logging
 import service
 import config
-
-from google.api import label_pb2 as ga_label
-from google.api import metric_pb2 as ga_metric
-from google.cloud import monitoring_v3
-
-
-# https://cloud.google.com/monitoring/docs/samples/monitoring-create-metric#monitoring_create_metric-python
-# https://cloud.google.com/monitoring/api/v3/kinds-and-types
-client = monitoring_v3.MetricServiceClient()
-project_name = f"projects/jkbff2"
-#descriptor = ga_metric.MetricDescriptor()
-#descriptor.type = "custom.googleapis.com/test/num_messages_received"
-#descriptor.metric_kind = ga_metric.MetricDescriptor.MetricKind.CUMULATIVE
-#descriptor.value_type = ga_metric.MetricDescriptor.ValueType.INT64
-#descriptor.description = "This is a simple example of a custom metric."
-
-#labels = ga_label.LabelDescriptor()
-#labels.key = "TestLabel"
-#labels.value_type = ga_label.LabelDescriptor.ValueType.STRING
-#labels.description = "This is a test label for my medium blog"
-#descriptor.labels.append(labels)
-
-#descriptor = client.create_metric_descriptor(
-#    name=project_name, metric_descriptor=descriptor
-#)
-
-series = monitoring_v3.TimeSeries()
-series.metric.type = "custom.googleapis.com/test/num_messages_received2"
-series.resource.type = "gke_container"
-series.resource.labels["zone"] = "us-west1-b"
-series.resource.labels["instance_id"] = "TODO"
-series.resource.labels["cluster_name"] = "cluster-2"
-series.resource.labels["namespace_id"] = "TODO"
-series.resource.labels["pod_id"] = "TODO"
-series.resource.labels["container_name"] = "my-container"
-
-series.metric.labels["Worker"] = config.DB_USERNAME()
-series.metric.labels["App"] = config.DB_NAME()
+import metrics
 
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
@@ -55,6 +18,8 @@ num_messages_received = 0
 alt_login_threshold_seconds = 15
 min_zone_id = config.MIN_ZONE_ID()
 max_zone_id = 10000000
+
+total_events_received_ts = metrics.create_time_series("total_events_received")
 
 
 def process_message(ws, message):
@@ -185,18 +150,10 @@ def verify_messages_received():
 
 
 def log_num_messages_received():
-    logger.info(f"messages received: {num_messages_received:,}")
+    #logger.info(f"messages received: {num_messages_received:,}")
     
     try:
-        now = time.time()
-        seconds = int(now)
-        nanos = int((now - seconds) * 10**9)
-        interval = monitoring_v3.TimeInterval(
-            {"end_time": {"seconds": seconds, "nanos": nanos}}
-        )
-        point = monitoring_v3.Point({"interval": interval, "value": {"int64_value": num_messages_received}})
-        series.points = [point]
-        client.create_time_series(name=project_name, time_series=[series])
+        metrics.publish_time_series(total_events_received_ts, num_messages_received)
     except Exception as e:
         logger.error("error sending metrics", exc_info=e)
 
---
+++ src/metrics.py
@@ -0,0 +1,57 @@
+import config
+import time
+
+#from google.api import label_pb2 as ga_label
+#from google.api import metric_pb2 as ga_metric
+from google.cloud import monitoring_v3
+
+
+# https://cloud.google.com/monitoring/docs/samples/monitoring-create-metric#monitoring_create_metric-python
+# https://cloud.google.com/monitoring/api/v3/kinds-and-types
+client = monitoring_v3.MetricServiceClient()
+project_name = f"projects/jkbff2"
+#descriptor = ga_metric.MetricDescriptor()
+#descriptor.type = "custom.googleapis.com/test/num_messages_received"
+#descriptor.metric_kind = ga_metric.MetricDescriptor.MetricKind.CUMULATIVE
+#descriptor.value_type = ga_metric.MetricDescriptor.ValueType.INT64
+#descriptor.description = "This is a simple example of a custom metric."
+
+#labels = ga_label.LabelDescriptor()
+#labels.key = "TestLabel"
+#labels.value_type = ga_label.LabelDescriptor.ValueType.STRING
+#labels.description = "This is a test label for my medium blog"
+#descriptor.labels.append(labels)
+
+#descriptor = client.create_metric_descriptor(
+#    name=project_name, metric_descriptor=descriptor
+#)
+
+def create_time_series(name):
+    # https://cloud.google.com/monitoring/custom-metrics/creating-metrics#writing-ts
+    series = monitoring_v3.TimeSeries()
+    series.metric.type = f"custom.googleapis.com/ps2-events-saver/{name}"
+
+    # https://cloud.google.com/monitoring/api/resources#tag_gke_container
+    series.resource.type = "gke_container"
+    series.resource.labels["zone"] = "us-west1-b"
+    series.resource.labels["instance_id"] = "TODO"
+    series.resource.labels["cluster_name"] = "cluster-2"
+    series.resource.labels["namespace_id"] = "TODO"
+    series.resource.labels["pod_id"] = "TODO"
+    series.resource.labels["container_name"] = config.get_env_string("HOSTNAME", "local")
+
+    series.metric.labels["application"] = "ps2-events-saver"
+    series.metric.labels["instance"] = config.DB_USERNAME()
+
+    return series
+
+def publish_time_series(series, value):
+    now = time.time()
+    seconds = int(now)
+    nanos = int((now - seconds) * 10**9)
+    interval = monitoring_v3.TimeInterval(
+        {"end_time": {"seconds": seconds, "nanos": nanos}}
+    )
+    point = monitoring_v3.Point({"interval": interval, "value": {"int64_value": value}})
+    series.points = [point]
+    client.create_time_series(name=project_name, time_series=[series])
